a377afed2b95b7af0da9798d7c15ca7c636def64
Natsuki0117 <natsu000117@icloud.com>
2025-08-08 03:49:17 +0900

    packageのバグで一時間くらい悩まされた；；；；； これは一晩寝かせたほうがいい案件です

---
+++ ToDoTask/AuthenticationManager.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationManager.swift
+//  ToDoTask
+//
+//  Created by 金井菜津希 on 2025/08/08.
+//
+
+import SwiftUI
+
+struct AuthenticationManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthenticationManager()
+}
