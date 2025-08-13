63ac961a7bb581154a0cefa6813d0e7995ab88ad
Natsuki0117 <natsu000117@icloud.com>
2025-08-14 00:47:47 +0900

    またまたログイン分岐しなくなるの、きちゃーーー accountviewが終わってるのと、たすくの非表示が一時的なので永続的になるように、一応消えはする、一時的に

---
+++ InitialSetupView.swift
@@ -0,0 +1,18 @@
+//
+//  InitialSetup.swift
+//  ToDoTask
+//
+//  Created by 金井菜津希 on 2025/08/14.
+//
+
+import SwiftUI
+
+struct InitialSetup: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InitialSetup()
+}
---
+++ TaskStore.swift
@@ -0,0 +1,18 @@
+//
+//  TaskStore.swift
+//  ToDoTask
+//
+//  Created by 金井菜津希 on 2025/08/14.
+//
+
+import SwiftUI
+
+struct TaskStore: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskStore()
+}
