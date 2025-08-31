15033c486b4df59c65e8760f093f6d509d51bd21
Natsuki0117 <natsu000117@icloud.com>
2025-09-01 04:34:56 +0900

    眠すぎるじゃんよーーー

---
+++ PieChartView.swift
@@ -0,0 +1,18 @@
+//
+//  PieChartView.swift
+//  ToDoTask
+//
+//  Created by 金井菜津希 on 2025/08/31.
+//
+
+import SwiftUI
+
+struct PieChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PieChartView()
+}
